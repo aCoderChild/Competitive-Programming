20c6d0e5d7b4d9dde5a99e30580fbcdbe4c1eb66
MaiAnhPham <167604421+aCoderChild@users.noreply.github.com>
2024-04-23 08:26:12 +0700

    Create 10071.cpp

---
+++ Competitive Programming Book 1/ch1/Getting Started/10071.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int v, t;
+    while(cin >> v >> t) {
+        cout << v * t * 2 << endl;
+    }
+
+    return 0;
+}
